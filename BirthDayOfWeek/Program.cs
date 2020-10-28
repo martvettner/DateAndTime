6fce7b1ae5579859ce8f478c82ca93d83308e736
mart vettner <martvettner@gmail.com>
2020-10-28 15:54:02 +0200

    Add project files.

---
+++ BirthDayOfWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+
+namespace BirthdayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("when were you born (dd.mm.yyyy)");
+            string userInput = Console.ReadLine();
+
+            BirthWeekCalculator(userInput);
+
+        }
+        public static void BirthWeekCalculator(string userInput)
+        {
+            string day = userInput.Substring(0, 2);
+            string month = userInput.Substring(3, 2);
+            string year = userInput.Substring(6, 4);
+
+            int dayInt = Int32.Parse(day);
+            int monthInt = Int32.Parse(month);
+            int yearInt = Int32.Parse(year);
+
+            DateTime BirthWeek = new DateTime(yearInt, monthInt, dayInt);
+            var birthWeekFinal = BirthWeek.DayOfWeek;
+            Console.WriteLine(birthWeekFinal);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DateAndTime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime(year, month, day, hour, minute)
+            
+
+            DateTime today = DateTime.Today;
+            Console.WriteLine($"Today: {today}");
+            int YearToday = today.Year;
+            Console.WriteLine($"Current year from today {YearToday}");
+            var weekDayToday = today.DayOfWeek;
+            Console.WriteLine($"it's {weekDayToday}!");
+
+            Console.WriteLine("");
+            DateTime todayDetails = DateTime.Now;
+            Console.WriteLine($"Today: {todayDetails}");
+
+            DateTime utc = DateTime.UtcNow;
+            Console.WriteLine($"time rn {utc}");
+
+            DateTime christmasEve = new DateTime(2020, 12, 24);
+            var chrsitmasDayOfweek = christmasEve.DayOfWeek;
+            Console.WriteLine(chrsitmasDayOfweek);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
